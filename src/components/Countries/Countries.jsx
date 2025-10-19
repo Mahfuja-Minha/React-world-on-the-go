e6440c48759077d948b64b8d57e5f828fdd75e7b
minha <mahfujaakterminha@gmail.com>
2025-10-19 13:07:49 +0600

    updated

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,21 @@
-const Countries = () => {
-    return (
-        <div>
-            <h1>In the country</h1>
-        </div>
-    );
+import { use } from "react";
+import Country from "../Country/Country";
+import './Countries.css'
+
+const Countries = ({ countriesPromise }) => {
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
+  console.log(countries);
+  return (
+    <div >
+      <h1>In the countries :{countries.length}</h1>
+      <div className="countries">
+        {countries.map((country) => (
+        <Country key={country.cca3.cca3} country={country}></Country>
+      ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
